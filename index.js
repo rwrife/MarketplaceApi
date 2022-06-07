e1d134825b88453735e0ae14a48cf37944d91b2b
Ryan Rife <ryrife@microsoft.com>
2022-06-06 22:30:02 -0700

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var serveStatic = require('serve-static');
+var data = require('./data.json');
+
+var staticBasePath = './static';
+
+var app = express();
+
+app.use(serveStatic(staticBasePath, {'index': false}));
+
+app.get('/api', (req, res) => {
+    res.send(JSON.stringify(data))
+})
+
+app.listen(8080);
